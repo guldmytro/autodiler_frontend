31afb10844ba74f3885507d499d86338c88ca45f
Dmytro <guldmytro@gmail.com>
2024-03-20 12:19:09 +0200

    Fixed terms

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,19 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 import { loadLocaleAsync } from '$i18n/i18n-util.async'
-import { PUBLIC_API_URL } from '$env/static/public';
+
 
 export const load: LayoutServerLoad = async ({ locals: { locale, LL } }) => {
-	const apiUrl = PUBLIC_API_URL.replace('[lang]', 'uk');
-	let terms = [];
-	try {
-		terms = await fetch(`${apiUrl}category/dump/`).then(r => r.json());
-	} catch(e) {
-		console.warn(e);
-	}
-	let termsWithImage = terms.filter(item => item.data && item.data.image);
-	let termsWithoutImage = terms.filter(item => !item.data || !item.data.image || item?.data?.image === '');
-	terms = termsWithImage.concat(termsWithoutImage);
-
 	await loadLocaleAsync(locale);
-	return { locale, terms };
+	return { locale };
 }
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,9 +2,19 @@ import type { LayoutLoad } from './$types'
 import type { Locales } from '$i18n/i18n-types'
 import { loadLocaleAsync } from '$i18n/i18n-util.async'
 import { i18nObject } from '$i18n/i18n-util.js'
+import { PUBLIC_API_URL } from '$env/static/public';
 
-export const load: LayoutLoad<{ locale: Locales }> = async ({ data: { locale, terms }, fetch }) => {
-	
+export const load: LayoutLoad<{ locale: Locales }> = async ({ data: { locale }, fetch }) => {
+	const apiUrl = PUBLIC_API_URL.replace('[lang]', 'uk');
+	let terms = [];
+	try {
+		terms = await fetch(`${apiUrl}category/dump/`).then(r => r.json());
+	} catch(e) {
+		console.warn(e);
+	}
+	let termsWithImage = terms.filter(item => item.data && item.data.image);
+	let termsWithoutImage = terms.filter(item => !item.data || !item.data.image || item?.data?.image === '');
+	terms = termsWithImage.concat(termsWithoutImage);
 	
 	// load dictionary into memory
 	await loadLocaleAsync(locale)
