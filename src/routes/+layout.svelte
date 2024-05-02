ec8b148fa5bdcc0b9babc25fe1c9ba80e870083f
Dmytro <guldmytro@gmail.com>
2024-05-02 13:29:24 +0300

    Removed fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,9 +33,6 @@
 	<HeadHrefLangs />
 	<meta name="robots" content="noindex">
 	<link rel="canonical" href={$page.url.toString()}>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
 <ProgressBar color="#5285cc" />
