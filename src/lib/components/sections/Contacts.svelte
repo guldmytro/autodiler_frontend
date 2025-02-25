7d1a7b571983cc6c4024f57b6614c46e30b75548
Dmytro <guldmytro@gmail.com>
2025-02-25 18:09:49 +0200

    Fixed schedule

--- src/lib/components/sections/Contacts.svelte
+++ src/lib/components/sections/Contacts.svelte
@@ -56,7 +56,7 @@
                         </div>            
                         <ul class="contacts-item__list">
                             <li class="contacts-item__element">
-                                м. Харків пр. Степний
+                                м. Харків, вул. Москалівська, 36
                             </li>
                             
                             <li class="contacts-item__element contacts-item__schedule">
