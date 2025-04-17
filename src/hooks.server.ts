ecdaf2f4858552cc56359cf387357231141c933f
Dmytro <guldmytro@gmail.com>
2025-04-17 14:53:52 +0300

    f

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -47,7 +47,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	// if slug is not a locale, use base locale (e.g. api endpoints)
-	const locale = isLocale(lang) ? (lang as Locales) : getPreferredLocale(event)
+	const locale = lang as Locales;
 	console.log(locale);
 	const LL = L[locale]
 
