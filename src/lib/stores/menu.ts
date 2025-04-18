4a2a9a2dc41617e6f629b3b3b29425fbb4c1007e
Dmytro <guldmytro@gmail.com>
2025-04-18 17:20:00 +0300

    f

--- src/lib/stores/menu.ts
+++ src/lib/stores/menu.ts
@@ -4,5 +4,5 @@ export let menu = writable(false);
 export let showTerms = writable(false);
 export let mobileMenu = writable(false);
 export let termsMenu = writable(false);
-export let searchMenu = writable(false);
+export let searchMenu = writable(true);
 export let takeOffer = writable(false);
