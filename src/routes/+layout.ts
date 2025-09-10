fd2737e6eccfed62ba628049a8cfa8910e7b6b1c
Dmytro <guldmytro@gmail.com>
2025-09-10 15:22:06 +0300

    f

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -10,6 +10,7 @@ export const load: LayoutLoad<{ locale: Locales }> = async ({ data: { locale }, 
 	let terms = [];
 	try {
 		terms = await fetch(`${apiUrl}category/dump/`).then(r => r.json());
+		terms = terms.filter(t => !('visible' in t?.data) || t?.data?.visible);
 		let termsWithImage = terms.filter(item => item.data && item.data.is_car_brand);
 		let termsWithoutImage = terms.filter(item => !item.data || !item.data.is_car_brand || item?.data?.is_car_brand === '');
 		terms = termsWithImage.concat(termsWithoutImage);
